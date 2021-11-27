1bd3fdffe462a95f0937d9896cb0b6097516864d
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2021-11-26 19:52:10 -0600

    added problem specific directions to each JS file

--- Problem 3/script.js
+++ Problem 3/script.js
@@ -1,8 +1,12 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the given number?
+
 // Original
 // function largestPrimeFactor(number) {
 
 //     const allFactors = [];
-    
+
 //     // Find all the factors of the given number
 //     // Store each of these factors in 'allFactors'
 //     for (let i = 1; i <= number; i++) {
@@ -31,24 +35,23 @@
 
 // largestPrimeFactor(13195);
 
-
 // Refactored
 function largestPrimeFactor(number) {
-    const primeFactors = [];
-    for (let i = 1; i <= number; i++) {
-        if (number % i === 0) {
-            let facStorage = [];
-            for (let j = 1; j <= i; j++) {
-                if (i % j === 0) {
-                    facStorage.push(j);
-                }
-            }
-            if (facStorage.length <= 2) {
-                primeFactors.push(i);
-            }
+  const primeFactors = [];
+  for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      let facStorage = [];
+      for (let j = 1; j <= i; j++) {
+        if (i % j === 0) {
+          facStorage.push(j);
         }
+      }
+      if (facStorage.length <= 2) {
+        primeFactors.push(i);
+      }
     }
-  return primeFactors[primeFactors.length-1];
+  }
+  return primeFactors[primeFactors.length - 1];
 }
 
 largestPrimeFactor(13195);
